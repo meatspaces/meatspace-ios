b646e8b7e96d734162429fc13d1cfda49eb3cf31
Marcus Ramberg <marcus.ramberg@usit.uio.no>
2016-03-22 21:05:06 +0100

    Add bridging header

---
+++ MeatChat/MeatChat2-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  MCPost-Bridging-Header.h
+//  MeatChat2
+//
+//  Created by Marcus Ramberg on 22/03/16.
+//  Copyright © 2016 Nordaaker AS. All rights reserved.
+//
+
+#ifndef MeatChat2_Bridging_Header_h
+#define MeatChat2_Bridging_Header_h
+#import "NSDate+TimeAgo.h"
+
+
+#endif /* MeatChat2_Bridging_Header_h */
